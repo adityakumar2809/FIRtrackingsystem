d6f12f6d04860004385df0e9dd0fec1af623d78c
Aditya Kumar Singh <42907777+adityakumar2809@users.noreply.github.com>
2020-06-29 00:43:31 +0530

    Change date format from YYYY-MM-DD to DD/MM/YY

--- FIRMonitoringSystem/settings.py
+++ FIRMonitoringSystem/settings.py
@@ -119,10 +119,11 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATE_INPUT_FORMATS = ['%m/%d/%y']
 
 LOGIN_URL = '/account/login'
 LOGIN_REDIRECT_URL = '/'
