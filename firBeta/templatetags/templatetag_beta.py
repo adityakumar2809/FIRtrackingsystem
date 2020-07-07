a1e36f4fffdc033beeef7d6b6709ee48e850c281
Aditya Kumar Singh <42907777+adityakumar2809@users.noreply.github.com>
2020-07-07 23:49:42 +0530

    Change limitation period expiry date based on appointed_io_date for phase_index other than 1

--- firBeta/templatetags/templatetag_beta.py
+++ firBeta/templatetags/templatetag_beta.py
@@ -8,7 +8,11 @@ register=template.Library()
 @register.filter
 def will_expire_at(pk):
     fir_phase = models.FIRPhase.objects.get(pk__exact=pk)
-    time_diff = (datetime.date.today() - fir_phase.date_registered).days
+    if fir_phase.phase_index == 1:
+        time_diff = (datetime.date.today() - fir_phase.date_registered).days
+    else:
+        fir_prev_phase = models.FIRPhase.objects.get(fir__exact = fir_phase.fir, phase_index__exact = fir_phase.phase_index - 1)
+        time_diff = (datetime.date.today() - fir_prev_phase.appointed_io_date).days
     if time_diff > fir_phase.limitation_period:
         return 0
     elif fir_phase.limitation_period - time_diff <= 10:
@@ -41,7 +45,11 @@ def is_third_phase(pk):
 @register.filter
 def get_expiry_date(pk):
     fir_phase = models.FIRPhase.objects.get(pk__exact=pk)
-    expiry_date = fir_phase.date_registered + datetime.timedelta(days = fir_phase.limitation_period) 
+    if fir_phase.phase_index == 1:
+        expiry_date = fir_phase.date_registered + datetime.timedelta(days = fir_phase.limitation_period)
+    else:
+        fir_prev_phase = models.FIRPhase.objects.get(fir__exact = fir_phase.fir, phase_index__exact = fir_phase.phase_index - 1)
+        expiry_date = fir_prev_phase.appointed_io_date + datetime.timedelta(days = fir_phase.limitation_period) 
     return expiry_date
 
 
