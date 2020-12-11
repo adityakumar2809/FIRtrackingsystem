bfa795e1a4bbc03a4e081298089e8b47afb15e6e
Aditya Kumar Singh <adityakumar2809@gmail.com>
2020-12-12 05:09:11 +0530

    Fix Bug in basic pagination for PS user viewing FIRs

--- firBeta/views.py
+++ firBeta/views.py
@@ -498,23 +498,23 @@ def list_edit_fir_ps_view(request, asc = 0):
         except:
             pass
 
-        # PAGINATION CODE STARTS
-        requested_page = request.GET.get('page', 1)
-        paginator_object = paginator.Paginator(fir_list, 20)
-        try:
-            paginated_fir_list = paginator_object.page(requested_page)
-        except paginator.PageNotAnInteger:
-            paginated_fir_list = paginator_object.page(1)
-        except paginator.EmptyPage:
-            paginated_fir_list = paginator_object.page(paginator_object.num_pages)
-        # PAGINATION CODE ENDS
-
         fir_combined_list = []
 
         for fir in paginated_fir_list:
             fir_phase_list = fir.phases.all()
             fir_combined_list.append([fir, fir_phase_list])
-        return render(request, 'firBeta/list_edit_fir_ps.html', {'fir_list': fir_combined_list, 'asc': asc, 'pagination_object': paginated_fir_list})
+
+        # PAGINATION CODE STARTS
+        requested_page = request.GET.get('page', 1)
+        paginator_object = paginator.Paginator(fir_combined_list, 20)
+        try:
+            paginated_fir_combined_list = paginator_object.page(requested_page)
+        except paginator.PageNotAnInteger:
+            paginated_fir_combined_list = paginator_object.page(1)
+        except paginator.EmptyPage:
+            paginated_fir_combined_list = paginator_object.page(paginator_object.num_pages)
+        # PAGINATION CODE ENDS
+        return render(request, 'firBeta/list_edit_fir_ps.html', {'fir_list': paginated_fir_combined_list, 'asc': asc, 'pagination_object': paginated_fir_combined_list})
     else:
         return redirect('fault', fault='ACCESS DENIED!')
 
